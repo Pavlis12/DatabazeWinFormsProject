75ff511a30994c71bebd935c3ac3125a299f1915
pavel svoboda <pavel97svoboda@seznam.cz>
2025-02-18 22:57:18 +0100

    First commit - add old code and recreate old UI.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1147, 352);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 403);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(190, 403);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(23, 471);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(141, 26);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(190, 471);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(141, 26);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(353, 471);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(344, 109);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(703, 471);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(64, 26);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(773, 471);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(141, 26);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(920, 471);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(141, 26);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(773, 540);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(141, 26);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(920, 540);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(141, 26);
+            this.textBox10.TabIndex = 10;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(920, 635);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(141, 26);
+            this.textBox11.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 31);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(508, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 31);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1067, 471);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 31);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1067, 540);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 31);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1067, 630);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 31);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 744);
+            this.ClientSize = new System.Drawing.Size(1204, 744);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,154 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Databaze
+namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
+        private DataTable table;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = true;
+            table = new DataTable();
+            table.Columns.Add("ID", typeof(string));
+            table.Columns.Add("Název", typeof(string));
+            table.Columns.Add("Popis", typeof(string));
+            table.Columns.Add("Kategorie", typeof(string));
+            table.Columns.Add("M/K", typeof(char));
+            table.Columns.Add("Počet", typeof(int));
+            dataGridView1.DataSource = table;
+        }
+
+        private void prBtn_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(PridPocetTxt.Text, out int pocet))
+            {
+                if (!string.IsNullOrEmpty(PridMKTxt.Text) && PridMKTxt.Text.Length == 1)
+                {
+                    char mk = PridMKTxt.Text[0];
+
+                    table.Rows.Add(PridIDTxt.Text, PridNazTxt.Text,
+                                   PridPopTxt.Text, PridKatTxt.Text, mk, pocet);
+
+                    PridIDTxt.Text = string.Empty;
+                    PridKatTxt.Text = string.Empty;
+                    PridMKTxt.Text = string.Empty;
+                    PridNazTxt.Text = string.Empty;
+                    PridPocetTxt.Text = string.Empty;
+                    PridPopTxt.Text = string.Empty;
+                }
+
+            }
+
+
+        }
+
+        private void hledBtn_Click(object sender, EventArgs e)
+        {
+
+            string hledatID = hledIdTxt.Text;
+            string hledatKat = hledKatTxt.Text;
+            string filtr = "";
+
+            if (string.IsNullOrEmpty(hledatID))
+            {
+                filtr += $"ID = '{hledatID}'";
+            }
+
+            if (!string.IsNullOrEmpty(hledatKat))
+            {
+
+                if (!string.IsNullOrEmpty(filtr))
+                {
+                    filtr += " AND ";
+                }
+                filtr += $"Kategorie = '{hledatKat}'";
+            }
+
+            if (!string.IsNullOrEmpty(filtr))
+            {
+                DataRow[] radky = table.Select(filtr);
+
+                if (radky.Length > 0)
+                {
+                    DataTable filtred = table.Clone();
+
+                    foreach (DataRow row in radky)
+                    {
+                        filtred.ImportRow(row);
+                    }
+
+                    dataGridView1.DataSource = filtred;
+
+                }
+            }
+            else
+            {
+                dataGridView1.DataSource = table;
+            }
+
+        }
+
+        private void ZpetBtn_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = table;
+        }
+
+        private void OdBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(odbIDtxt.Text) && int.TryParse(odbPocetTxt.Text, out int novyPocet))
+            {
+
+                string HledanNazev = odbIDtxt.Text;
+                foreach (DataRow row in table.Rows)
+                {
+                    if ((string)row["ID"] == HledanNazev)
+                    {
+                        row["Počet"] = novyPocet;
+                        break;
+                    }
+
+                }
+
+            }
+        }
+
+        private void OdsBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(odbIDtxt.Text))
+            {
+
+                string hledanNazev = odbIDtxt.Text.Trim().Replace("'", "''");
+                DataRow[] vymazat = table.Select($"Convert(ID, 'System.String') LIKE '{hledanNazev}'");
+                if (vymazat.Length > 0)
+                {
+                    foreach (DataRow row in vymazat)
+                    {
+                        row.Delete();
+                    }
+                    table.AcceptChanges();
+
+
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = table;
+                }
+
+            }
         }
     }
 }
